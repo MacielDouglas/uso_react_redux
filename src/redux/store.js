5f84f3f44a8afe6793db8d45c8e50ce966f96688
MacielDouglas <douglasmaciel.dm@gmail.com>
2023-08-22 11:45:14 -0300

    Utilizado a biblioteca redux/toolkit, e feito as modificações necessárias

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,12 @@
-// const { createStore } = require('redux');
-import { applyMiddleware, createStore } from 'redux';
+// import { applyMiddleware, createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import rootReducer from './root-reducer';
 import logger from 'redux-logger';
 
-const store = createStore(rootReducer, applyMiddleware(logger));
+// const store = createStore(rootReducer, applyMiddleware(logger));
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: [logger],
+});
 
 export default store;
